ea326522ef3457ca55ee3c7862c1eb894549cf2f
pskepslilly <140721439+pskepslilly@users.noreply.github.com>
2023-10-11 16:11:14 -0400

    Update setup.py
    Changed the Flask dependency issues so they can be resolved using python3.8

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ setup(
     tests_require=['pytest', 'pytest-cov', 'pytest-flask'],
     install_requires=[
         'Flask>=1.0.3',
-        'Flask-SQLAlchemy==2.4.0',
-        'SQLAlchemy==1.3.3',
+        'Flask-SQLAlchemy>=2.4.0',
+        'SQLAlchemy>=1.3.3',
         'Flask-Admin>=1.5.3',
         'Flask-HTTPAuth>=3.2.4',
         ],
